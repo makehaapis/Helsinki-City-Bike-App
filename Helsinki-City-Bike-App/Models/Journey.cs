d4b714b40c5bc3dab3f3d9c4fdbeb9ff66e62ebf
makehaapis <marko1927@student.hamk.fi>
2023-01-04 18:56:30 +0200

    Station and Journey models created

---
+++ Helsinki-City-Bike-App/Models/Journey.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Helsinki_City_Bike_App.Models
+{
+    public class Journey
+    {
+        [Key]
+        public int JourneyID { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ReturnTime { get; set; }
+        public int Distance { get; set; }
+        public float Duration { get; set; }
+
+        [ForeignKey(nameof(ReturnStationID))]
+        public int ReturnStationID { get; set; }
+        public Station ReturnStation { get; set; }
+        [ForeignKey(nameof(DepartureStationID))]
+        public int DepartureStationID { get; set; }
+        public Station DepartureStation { get; set; }
+    }
+}
---
+++ Helsinki-City-Bike-App/Models/Station.cs
@@ -0,0 +1,19 @@
+﻿using Helsinki_City_Bike_App.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Helsinki_City_Bike_App.Models
+{ }
+    public class Station
+    {
+        //Allow us to add primary key from csv.
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int StationID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Capacity { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+        public ICollection<Journey> DepartingJourneys { get; set; }
+        public ICollection<Journey> ReturningJourneys { get; set; }
+    }
+}
