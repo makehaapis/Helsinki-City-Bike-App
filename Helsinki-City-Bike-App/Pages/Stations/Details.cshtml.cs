2aa1719e535cd19ed105aa811b220c7198afe9aa
makehaapis <marko1927@student.hamk.fi>
2023-01-09 17:28:43 +0200

    Updating station detail page

--- Helsinki-City-Bike-App/Pages/Stations/Details.cshtml.cs
+++ Helsinki-City-Bike-App/Pages/Stations/Details.cshtml.cs
@@ -12,30 +12,56 @@ namespace Helsinki_City_Bike_App.Pages.Stations
 {
     public class DetailsModel : PageModel
     {
-        private readonly Helsinki_City_Bike_App.Data.AppDbContext _context;
+        private readonly AppDbContext _context;
 
-        public DetailsModel(Helsinki_City_Bike_App.Data.AppDbContext context)
+        public DetailsModel(AppDbContext context)
         {
             _context = context;
         }
 
-      public Station Station { get; set; }
+        public Station Station { get; set; }
+        public List<Journey> DepartingJourneys { get; set; }
+        public List<Journey> ReturningJourneys { get; set; }
+        public int AverageStartingJourneyDistance { get; set; }
+        public int AverageReturnJourneyDistance { get; set; }
+        public int TotalStartingJourneyDistance { get; set; }
+        public int TotalReturningJourneyDistance { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+
+        public async Task<IActionResult> OnGetAsync(int? id, int? JourneyID)
         {
             if (id == null || _context.Stations == null)
             {
                 return NotFound();
             }
-
-            var station = await _context.Stations.FirstOrDefaultAsync(m => m.StationID == id);
-            if (station == null)
+            Station = await _context.Stations.Include(s => s.ReturningJourneys).Include(s => s.DepartingJourneys).FirstOrDefaultAsync(m => m.StationID == id);
+            if (Station == null)
             {
                 return NotFound();
             }
-            else 
+            else
             {
-                Station = station;
+                Station = Station;
+                DepartingJourneys = _context.Journeys.Where(j => j.DepartureStationID.Equals(id)).ToList();
+                ReturningJourneys = _context.Journeys.Where(j => j.ReturnStationID.Equals(id)).ToList();
+
+                if (DepartingJourneys.Count() > 0)
+                {
+                    foreach (var item in DepartingJourneys)
+                    {
+                        TotalStartingJourneyDistance += item.Distance;
+                    }
+                    AverageStartingJourneyDistance = TotalStartingJourneyDistance / DepartingJourneys.Count();
+                }
+
+                if (ReturningJourneys.Count() > 0)
+                {
+                    foreach (var item in ReturningJourneys)
+                    {
+                        TotalReturningJourneyDistance += item.Distance;
+                    }
+                    AverageReturnJourneyDistance = TotalReturningJourneyDistance / ReturningJourneys.Count();
+                }
             }
             return Page();
         }
