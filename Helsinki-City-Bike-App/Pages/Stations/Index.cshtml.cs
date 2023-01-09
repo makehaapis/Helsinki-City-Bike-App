156ec5df01a4edc73a239965a6ce85906a29e87b
makehaapis <marko1927@student.hamk.fi>
2023-01-09 17:03:58 +0200

    Updating stations index page and adding pagination

--- Helsinki-City-Bike-App/Pages/Stations/Index.cshtml.cs
+++ Helsinki-City-Bike-App/Pages/Stations/Index.cshtml.cs
@@ -12,22 +12,66 @@ namespace Helsinki_City_Bike_App.Pages.Stations
 {
     public class IndexModel : PageModel
     {
-        private readonly Helsinki_City_Bike_App.Data.AppDbContext _context;
-
-        public IndexModel(Helsinki_City_Bike_App.Data.AppDbContext context)
+        private readonly AppDbContext _context;
+        public IndexModel(AppDbContext context)
         {
             _context = context;
         }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+        public string NameSort { get; set; }
+        public string AddressSort { get; set; }
+        public string CapacitySort { get; set; }
+        public PaginatedList<Station> Stations { get; set; } = default!;
 
-        public IList<Station> Station { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
-            if (_context.Stations != null)
+            CurrentSort = sortOrder;
+            NameSort = sortOrder == "StationName" ? "station_name_desc" : "StationName";
+            AddressSort = sortOrder == "StationAddress" ? "station_address_desc" : "StationAddress";
+            CapacitySort = sortOrder == "StationCapacity" ? "station_capacity_desc" : "StationCapacity";
+
+            if (searchString != null)
             {
-                Station = await _context.Stations.ToListAsync();
+                pageIndex = 1;
             }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            currentFilter = searchString;
+            IQueryable<Station> stationsIQ = from s in _context.Stations
+                                             select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                stationsIQ = stationsIQ.Where(s => s.Name.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "StationName":
+                    stationsIQ = stationsIQ.OrderBy(s => s.Name);
+                    break;
+                case "station_name_desc":
+                    stationsIQ = stationsIQ.OrderByDescending(s => s.Name);
+                    break;
+                case "StationAddress":
+                    stationsIQ = stationsIQ.OrderBy(j => j.Address);
+                    break;
+                case "station_address_desc":
+                    stationsIQ = stationsIQ.OrderByDescending(j => j.Address);
+                    break;
+                case "StationCapacity":
+                    stationsIQ = stationsIQ.OrderBy(j => j.Capacity);
+                    break;
+                case "station_capacity_desc":
+                    stationsIQ = stationsIQ.OrderByDescending(j => j.Capacity);
+                    break;
+            }
+            var pageSize = 20;
+            Stations = await PaginatedList<Station>.CreateAsync(
+                stationsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
-    
