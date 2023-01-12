d548cc5f381a0c1012e62a5c4d7999db4a9fe6c7
makehaapis <marko1927@student.hamk.fi>
2023-01-12 12:11:44 +0200

    Seeding database with Journeys test set.

--- Helsinki-City-Bike-App/Data/AppDbInitializer.cs
+++ Helsinki-City-Bike-App/Data/AppDbInitializer.cs
@@ -1,9 +1,14 @@
 ﻿using Helsinki_City_Bike_App.Models;
+using Helsinki_City_Bike_App.Pages.Stations;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace Helsinki_City_Bike_App.Data
 {
     public class AppDbInitializer
     {
+        private static List<Journey> journeys;
+
         public static void Seed(AppDbContext context)
         {
             context.Database.EnsureCreated();
@@ -30,6 +35,60 @@ namespace Helsinki_City_Bike_App.Data
                     }
                 });
                 context.SaveChanges();
+            }
+
+            if (!context.Journeys.Any())
+            {
+                journeys = new List<Journey>();
+                journeys.AddRange(new List<Journey>()
+                {
+                    new Journey() {
+                        DepartureTime =DateTime.Parse("2021-05-31T23:57:25"),
+                        ReturnTime = DateTime.Parse("2021-06-01T00:05:46"),
+                        Distance = 12,
+                        Duration = 20,
+                        ReturnStationID = 501,
+                        DepartureStationID = 503
+                    },
+                    new Journey() {
+                        DepartureTime =DateTime.Parse("2021-05-31T23:57:25"),
+                        ReturnTime = DateTime.Parse("2021-06-01T00:05:46"),
+                        Distance = 12,
+                        Duration = 20,
+                        ReturnStationID = 501,
+                        DepartureStationID = 50
+                    },
+                    new Journey() {
+                        DepartureTime =DateTime.Parse("2023-05-31T23:57:25"),
+                        ReturnTime = DateTime.Parse("2023-06-01T00:05:46"),
+                        Distance = 12,
+                        Duration = 20,
+                        ReturnStationID = 501,
+                        DepartureStationID = 503
+                    },
+                    new Journey() {
+                        DepartureTime =DateTime.Parse("2022-05-31T23:57:25"),
+                        ReturnTime = DateTime.Parse("2022-06-01T00:05:46"),
+                        Distance = 12,
+                        Duration = 20,
+                        ReturnStationID = 501,
+                        DepartureStationID = 503
+                    }
+                });
+                foreach (var item in journeys)
+                {
+                    try
+                    {
+                        context.Journeys.Add(item);
+                        context.SaveChanges();
+                    }
+                    catch(DbUpdateException) 
+                    {
+                        context.Journeys.Remove(item);
+                        continue;
+                    }
+                    
+                }
             }
         }
     }
