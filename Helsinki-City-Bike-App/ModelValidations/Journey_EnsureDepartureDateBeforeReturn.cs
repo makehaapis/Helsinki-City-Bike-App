8deaea540a17ac0d3092b7598e55f3f32e440c3d
makehaapis <marko1927@student.hamk.fi>
2023-01-12 14:20:46 +0200

    Validation attribute to ensure journeys departure date is before return

---
+++ Helsinki-City-Bike-App/ModelValidations/Journey_EnsureDepartureDateBeforeReturn.cs
@@ -0,0 +1,18 @@
+﻿using Helsinki_City_Bike_App.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Helsinki_City_Bike_App.ModelValidations
+{
+    public class Journey_EnsureDepartureDateBeforeReturn: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var journey = validationContext.ObjectInstance as Journey;
+            if (journey != null && journey.DepartureTime < journey.ReturnTime) 
+            {
+                return new ValidationResult("Departure has to be before return!");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
