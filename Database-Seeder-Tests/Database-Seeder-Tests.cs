fd869a5713efac85f230c5be459a0b818263cc5d
Marko Haapasaari <marko1927@student.hamk.fi>
2023-02-07 15:47:29 +0200

    adding test cases

--- Database-Seeder-Tests/Database-Seeder-Tests.cs
+++ Database-Seeder-Tests/Database-Seeder-Tests.cs
@@ -29,7 +29,7 @@ namespace Database_Seeder_Tests
         }
 
         [Test]
-        public void ReturnTimeBeforeDeparture_ReturnFalse()
+        public void JourneyReturnTimeBeforeDeparture_ReturnFalse()
         {
             journey = new Journey();
             journey.JourneyID = 1;
@@ -42,6 +42,38 @@ namespace Database_Seeder_Tests
 
             bool result = ValidateCsvJourney.ValidateJourney(journey);
             Assert.IsFalse(result, "Return time not valid");
+        }
+
+        [Test]
+        public void JourneyDistanceTooShort_ReturnFalse()
+        {
+            journey = new Journey();
+            journey.JourneyID = 1;
+            journey.DepartureTime = DateTime.Parse("2021-07-31T23:59:59");
+            journey.ReturnTime = DateTime.Parse("2021-08-01T00:09:15");
+            journey.DepartureStationID = 113;
+            journey.ReturnStationID = 78;
+            journey.Distance = 9;
+            journey.Duration = 553;
+
+            bool result = ValidateCsvJourney.ValidateJourney(journey);
+            Assert.IsFalse(result, "Journey distance not valid");
+        }
+
+        [Test]
+        public void JourneyDurationTooShort_ReturnFalse()
+        {
+            journey = new Journey();
+            journey.JourneyID = 1;
+            journey.DepartureTime = DateTime.Parse("2021-07-31T23:59:59");
+            journey.ReturnTime = DateTime.Parse("2021-08-01T00:09:15");
+            journey.DepartureStationID = 113;
+            journey.ReturnStationID = 78;
+            journey.Distance = 19;
+            journey.Duration = 9;
+
+            bool result = ValidateCsvJourney.ValidateJourney(journey);
+            Assert.IsFalse(result, "Journey duration not valid");
         }
     }
 }
